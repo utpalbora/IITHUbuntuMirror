e1d9671fe3c3a798875324e636cffab770339911
Utpal Bora <cs14mtech11017@iith.ac.in>
2019-05-09 11:59:24 +0530

    No need to escape all the special characters

--- IITHUbuntuMirror.sh
+++ IITHUbuntuMirror.sh
@@ -2,8 +2,8 @@
 
 #change mirror
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.`date '+%Y-%m-%d:%H:%M:%S'`
-sudo sed -ri "s/[a-z]\{2\}[a-z]?\.archive\.ubuntu\.com/mirror\.iith\.ac\.in/" /etc/apt/sources.list
-sudo sed -ri "s/security\.ubuntu\.com/mirror\.iith\.ac\.in/" /etc/apt/sources.list
+sudo sed -ri "s/[a-z]{2}[a-z]?.archive.ubuntu.com/mirror.iith.ac.in/" /etc/apt/sources.list
+sudo sed -ri "s/security.ubuntu.com/mirror.iith.ac.in/" /etc/apt/sources.list
 
 sudo apt-get update
 #sudo apt-get dist-upgrade -fmy
